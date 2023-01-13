fd7f6ae47633f4d1467cd61ea546452c85ded739
Maycon Jardim <mayconsmill@gmail.com>
2023-01-13 19:53:34 -0300

    Work Order delete OK! DatabaseException class

--- backend/src/main/java/com/wom/api/services/WorkOrderService.java
+++ backend/src/main/java/com/wom/api/services/WorkOrderService.java
@@ -3,6 +3,8 @@ package com.wom.api.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.wom.api.dto.WorkOrderDTO;
 import com.wom.api.entities.WorkOrder;
 import com.wom.api.repositories.WorkOrderRepository;
+import com.wom.api.services.exceptions.DatabaseException;
 import com.wom.api.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -53,6 +56,16 @@ public class WorkOrderService {
 			return new WorkOrderDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found! " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			orderRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found! " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Database integrity violation!");
 		}
 	}
 
