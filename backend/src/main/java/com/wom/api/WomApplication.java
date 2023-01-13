3aa328314dcc489f40f15f6ae1664406b0903bb5
Maycon Jardim <mayconsmill@gmail.com>
2023-01-13 12:32:50 -0300

    Backend project created

---
+++ backend/src/main/java/com/wom/api/WomApplication.java
@@ -0,0 +1,13 @@
+package com.wom.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wom/api/WomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wom.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
