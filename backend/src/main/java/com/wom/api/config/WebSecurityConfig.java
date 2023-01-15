e8032da8e04b6df45d64c67cc24e5c2b18bf321f
Maycon Jardim <mayconsmill@gmail.com>
2023-01-15 20:35:40 -0300

    Permit all requests temporarily

--- backend/src/main/java/com/wom/api/config/WebSecurityConfig.java
+++ backend/src/main/java/com/wom/api/config/WebSecurityConfig.java
@@ -2,7 +2,9 @@ package com.wom.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
@@ -10,6 +12,13 @@ public class WebSecurityConfig {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests(auth -> {
+			auth.anyRequest().permitAll();
+		}).build();
 	}
 
 }
