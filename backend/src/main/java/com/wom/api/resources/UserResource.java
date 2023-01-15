420d00a05890e15c6523c0b1f41732abc94e7de5
Maycon Jardim <mayconsmill@gmail.com>
2023-01-15 11:47:05 -0300

    UserInsertDTO

--- backend/src/main/java/com/wom/api/resources/UserResource.java
+++ backend/src/main/java/com/wom/api/resources/UserResource.java
@@ -1,15 +1,21 @@
 package com.wom.api.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wom.api.dto.UserDTO;
+import com.wom.api.dto.UserInsertDTO;
 import com.wom.api.services.UserService;
 
 @RestController
@@ -31,6 +37,13 @@ public class UserResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto){
+		UserDTO newDto = userService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+		buildAndExpand(newDto.getId()).toUri(); 
+		return ResponseEntity.created(uri).body(newDto); 
+	}
 
 	
 }
--- backend/src/main/java/com/wom/api/services/UserService.java
+++ backend/src/main/java/com/wom/api/services/UserService.java
@@ -8,8 +8,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.wom.api.dto.RoleDTO;
 import com.wom.api.dto.UserDTO;
+import com.wom.api.dto.UserInsertDTO;
+import com.wom.api.entities.Role;
 import com.wom.api.entities.User;
+import com.wom.api.repositories.RoleRepository;
 import com.wom.api.repositories.UserRepository;
 import com.wom.api.services.exceptions.ResourceNotFoundException;
 
@@ -18,6 +22,9 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
 
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findAllPaged(Pageable pageable) {
@@ -30,6 +37,28 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found! " + id));
 		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO insert(UserInsertDTO dto) {
+		User entity = new User();
+		copyDtoEntity(dto, entity);
+		entity.setPassword((dto.getPassword()));
+		entity = userRepository.save(entity);
+		return new UserDTO(entity);
+	}
+
+	private void copyDtoEntity(UserDTO dto, User entity) {
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setEmail(dto.getEmail());
+		entity.setPhoneNumber(dto.getPhoneNumber());
+
+		entity.getRoles().clear();
+		for (RoleDTO roleDto : dto.getRoles()) {
+			Role role = roleRepository.getReferenceById(roleDto.getId());
+			entity.getRoles().add(role);
+		}
 	}
 
 }
