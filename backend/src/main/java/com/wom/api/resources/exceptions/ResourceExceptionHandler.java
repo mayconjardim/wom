6318351211692a7d8a5ec29dfacd3281002a93ff
Maycon Jardim <mayconsmill@gmail.com>
2023-01-18 15:38:08 -0300

    Handling validation exceptions

--- backend/src/main/java/com/wom/api/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/wom/api/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,7 +19,7 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandartError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-		HttpStatus status = HttpStatus.NOT_FOUND; // 404
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandartError err = new StandartError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
@@ -29,13 +31,30 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(DatabaseException.class)
 	public ResponseEntity<StandartError> database(DatabaseException e, HttpServletRequest request) {
-		HttpStatus status = HttpStatus.BAD_REQUEST; // 400
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandartError err = new StandartError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("Database exception!");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation exception!");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+
 		return ResponseEntity.status(status).body(err);
 	}
 
