46797cfefbe031bdb740334008e426880dc7bbed
Maycon Jardim <mayconsmill@gmail.com>
2023-01-19 13:35:10 -0300

    Order List component

--- frontend/src/app/features/order/order.module.ts
+++ frontend/src/app/features/order/order.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderListComponent } from './order-list/order-list.component';
 
-export const routes: Routes = [{ path: '' }];
+export const routes: Routes = [{ path: '', component: OrderListComponent }];
 
 @NgModule({
-  declarations: [],
+  declarations: [OrderListComponent],
   imports: [CommonModule, RouterModule.forChild(routes), FormsModule],
 })
 export class OrderModule {}
