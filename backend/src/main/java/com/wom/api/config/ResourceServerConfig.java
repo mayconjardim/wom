19b89118d8040469606df10e299df41554eac6ea
Maycon Jardim <mayconsmill@gmail.com>
2023-01-21 19:39:40 -0300

    Applying method security @Secured in backend

--- backend/src/main/java/com/wom/api/config/ResourceServerConfig.java
+++ backend/src/main/java/com/wom/api/config/ResourceServerConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -22,6 +22,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Value("${cors.origins}")
@@ -29,14 +30,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private JwtTokenStore tokenStore;
-	
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**", "/orders/**" };
-	private static final String[] MANAGER_OR_ADMIN_OR_YARD = {};
-	private static final String[] ADMIN = {"/users/**"};
-	private static final String[] USER = {"/users"};
+
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**", };
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -46,43 +44,33 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 
-		
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
-		http.authorizeRequests()
-		.antMatchers(PUBLIC).permitAll() 
-		.antMatchers(HttpMethod.POST, USER).permitAll()
-		.antMatchers(HttpMethod.GET, USER).permitAll()
-		.antMatchers(HttpMethod.GET, MANAGER_OR_ADMIN_OR_YARD).permitAll() 
-		.antMatchers(HttpMethod.POST, MANAGER_OR_ADMIN_OR_YARD).permitAll()
-		.antMatchers(HttpMethod.PUT, MANAGER_OR_ADMIN_OR_YARD).permitAll() 
-		.antMatchers(MANAGER_OR_ADMIN_OR_YARD).hasAnyRole("MANAGER", "ADMIN", "YARD") 
-		.antMatchers(ADMIN).hasRole("ADMIN") 
-		.anyRequest().authenticated(); 
-		
+
+		http.authorizeRequests().antMatchers(PUBLIC).permitAll().anyRequest().authenticated();
+
 		http.cors().configurationSource(corsConfigurationSource());
 	}
-	
+
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration corsConfig  = new CorsConfiguration();
+		CorsConfiguration corsConfig = new CorsConfiguration();
 		corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
 		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
 		corsConfig.setAllowCredentials(true);
 		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "*"));
-		
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", corsConfig);
 		return source;
 	}
-	
+
 	@Bean
 	FilterRegistrationBean<CorsFilter> corsFilter() {
-	    FilterRegistrationBean<CorsFilter> bean
-	            = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
-	    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-	    return bean;
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+				new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 }
--- backend/src/main/java/com/wom/api/resources/WorkOrderResource.java
+++ backend/src/main/java/com/wom/api/resources/WorkOrderResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,7 +39,9 @@ public class WorkOrderResource {
 		return ResponseEntity.ok().body(dto);
 	}
 
+	
 	@PostMapping
+	@Secured(value = {"ROLE_MANAGER", "ROLE_ADMIN"})
 	public ResponseEntity<WorkOrderDTO> insert(@RequestBody WorkOrderDTO dto) {
 		dto = orderSevice.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
@@ -46,12 +49,14 @@ public class WorkOrderResource {
 	}
 
 	@PutMapping(value = "/{id}")
+	@Secured(value = {"ROLE_YARD", "ROLE_MANAGER", "ROLE_ADMIN"})
 	public ResponseEntity<WorkOrderDTO> update(@PathVariable Long id, @RequestBody WorkOrderDTO dto) {
 		dto = orderSevice.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 
 	@DeleteMapping(value = "/{id}")
+	@Secured(value = {"ROLE_MANAGER", "ROLE_ADMIN"})
 	public ResponseEntity<WorkOrderDTO> delete(@PathVariable Long id) {
 		orderSevice.delete(id);
 		return ResponseEntity.noContent().build();
