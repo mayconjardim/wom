66edb90b518669d234055b2690d6de3413f00178
Maycon Jardim <mayconsmill@gmail.com>
2023-01-24 13:53:04 -0300

    User service, find all users list

--- frontend/src/app/features/users/services/user.service.ts
+++ frontend/src/app/features/users/services/user.service.ts
@@ -1,10 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { API_CONFIG } from 'src/app/core/config/api.config';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-constructor() { }
-
+  findAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${API_CONFIG.baseUrl}/users/all`);
+  }
 }
