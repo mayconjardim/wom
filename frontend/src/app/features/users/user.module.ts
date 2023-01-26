c82e8010b1613309ad0800d31a521aec6d3b752d
Maycon Jardim <mayconsmill@gmail.com>
2023-01-26 13:38:23 -0300

    User list component

--- frontend/src/app/features/users/user.module.ts
+++ frontend/src/app/features/users/user.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 export const routes: Routes = [{ path: '' }];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserListComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class UserModule {}
